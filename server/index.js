e89aa18f3173d5b87d21e0c8ec861cf6a7e10992
ArtisAdemi <artis.ademi@gmail.com>
2024-02-26 18:14:28 +0100

    Got some dependencies and started the sequelize structure

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
-const express = require('express');
+const db = require("./models");// Getting models from ./models
 
-console.log("Hello World")
+// Creating sequelize sync with db
+db.sequelize.sync().then(() => {
+    // After sync is complete we start server
+    app.listen(3001, () => {
+        console.log(`Server started on port: 3001`);
+    });
+});
