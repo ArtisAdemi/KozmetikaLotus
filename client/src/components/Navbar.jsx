f59672977dd9d5c44b367d7a23e6403bbf868f9a
ArtisAdemi <artis.ademi@gmail.com>
2024-03-05 13:14:37 +0100

    Started on Home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import LotusLogo from '../socialsLogo/LotusLogo'
 
 const Navbar = () => {
   return (
-    <div className='navbar-container flex justify-between bg-[#faf9f5] w-[80%] border-b-[2px] border-[#ABABAB] p-4'>
-        <div className='text-3xl font-bold font-[Poppins]'>
-            LOGO
+    <div className='navbar-container flex justify-between bg-transparent w-[80%] border-b-[2px] border-[#ABABAB] p-4'>
+        <div className='w-1/3'>
+            <LotusLogo />
         </div>
-        <div className='flex justify-between'>
+        <div className='flex justify-between w-1/3 items-center'>
             <div className='m-2'>
                 <p><a href="/">Home</a></p>
             </div>
@@ -17,7 +18,7 @@ const Navbar = () => {
                 <p><a href="/about">About Us</a></p>
             </div>
         </div>
-        <div className='navbar-right border-[2px] border-[#0C0C0C] px-5 py-2'>
+        <div className='navbar-right border-[2px] border-[#0C0C0C] px-5 py-2 items-center'>
             <button><a href="/contact">Contact Us</a></button>
         </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
-import { ProductSlider } from '../components'
+import { Navbar, ProductSlider } from '../components'
+import Star from '../socialsLogo/Star'
+import Divider from '../socialsLogo/Divider'
 
 const Home = () => {
   return (
     <div>
-      <div>Home</div>
-      <ProductSlider category={"lipstick"}/>
+      <div className='bg-[#F6CBD2] w-full'>
+        <div className='flex w-full justify-center'>
+          <Navbar />
+        </div>
+        <div className='flex justify-center'>
+          <img src={require('../images/HomePage1.png')} alt="IMAGE 1" />
+        </div>
+        <div className='flex w-full justify-center'>
+          <div className='flex justify-center w-[80%]'>
+            <div className='flex w-1/3'>
+              <div>
+                <Star />
+              </div>
+              <div>
+                <p className='w-[60%]'>
+                  Gjeni në internet produkte për kujdesin natyral të lëkurës, për trupin, për kujdesin e flokëve dhe kozmetikë.
+                </p>
+              </div>
+            </div>
+            <div className='w-1/3 flex items-center'>
+              <Divider />
+            </div>
+            <div className='w-1/3 flex items-center'>
+              <a href="/">Eksploroni te gjitha Produktet -></a>
+            </div>
+          </div>
+        </div>
+      </div>
+        <ProductSlider category={"lipstick"}/>
     </div>
   )
 }
