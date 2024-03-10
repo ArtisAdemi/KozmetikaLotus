a8f63404f384a2d35a3adac68d94ac76ea318560
Florian <florian.fani@hotmail.com>
2024-03-10 22:56:29 +0100

    mailer function ui

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-import { Navbar } from '../components'
-import contact from "../images/ContactUs.png"
+import React from 'react';
+import { useState } from 'react';
+import { Navbar } from '../components';
+import contact from "../images/ContactUs.png";
+import MailerService from '../services/Mailer';
+import { useNavigate } from 'react-router-dom';
 
 const ContactUs = () => {
-  
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleContactUs = async () => {
+    if (!userData.name || !userData.email || !userData.phone || !userData.message) {
+      alert('Please fill in all required fields.');
+      return;
+    }
+    try {
+      await MailerService.sendEmail(userData);
+      alert('Email sent successfully!');
+      navigate('/')
+    } catch (error) {
+      console.error('Error sending email:', error);
+      alert('Error sending email. Please try again.');
+    }
+  };
+
   return (
     <div>
       <div className='flex w-full justify-center'>
@@ -11,17 +42,17 @@ const ContactUs = () => {
       </div>
 
       <div className='contact-container w-full flex justify-center my-12'>
-        <div className='contact-content w-[80%] shadow-2xl justify-center  flex' style={{ height: '800px' }}>
+        <div className='contact-content w-[80%] shadow-2xl justify-center flex' style={{ height: '800px' }}>
           <div className='w-[100%] flex flex-col bg-[#FBEFF2] gap-10 p-5 justify-center'>
 
             <h1 className='text-[#A10550] text-3xl text-center'>Contact Us</h1>
-            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="text" name="name" placeholder='Name' id="" required />
-            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="email" name="email" placeholder='Email' id="" required />
-            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="number" name="number" placeholder='Number' id="" required />
+            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="text" name="name" placeholder='Name' id="" required onChange={handleInputChange} />
+            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="email" name="email" placeholder='Email' id="" required onChange={handleInputChange} />
+            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="number" name="phone" placeholder='Phone' id="" required onChange={handleInputChange} />
             
-            <textarea style={{resize: 'none'}} className='rounded-md p-4 w-[60%] mx-auto' name="textarea" placeholder='Write something...' id="" cols="40" rows="5"></textarea>
+            <textarea style={{resize: 'none'}} className='rounded-md p-4 w-[60%] mx-auto' name="message" placeholder='Write something...' id="" cols="40" rows="5" onChange={handleInputChange}></textarea>
 
-            <button className='border-black border-2 p-3 w-[30%] mx-auto text-[#0C0C0C] shadow-xl'>Contact Us</button>
+            <button className='border-black border-2 p-3 w-[30%] mx-auto text-[#0C0C0C] shadow-xl' onClick={handleContactUs}>Contact Us</button>
 
           </div>
 
@@ -32,7 +63,7 @@ const ContactUs = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
---
+++ client/src/services/Mailer.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+//import buildUrl from '../middleware/BuildParam';
+const API_URL = 'http://localhost:3001/api/mailer';
+
+const MailerService = {
+    sendEmail: async (userData) => {
+        try {
+            const response = await axios.post(`${API_URL}`, userData);
+            console.log('Email sent successfully:', response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error sending email:', error);
+            throw error;
+        }
+    }
+
+    
+};
+
+export default MailerService;
