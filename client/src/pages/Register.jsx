a1759b2f189a37c2881a4a645674f29e5f334756
Florian <florian.fani@hotmail.com>
2024-03-12 13:55:48 +0100

    form initialized

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,17 +5,20 @@ const Register = () => {
         <div>
             <div className='contact-container w-full flex justify-center my-12'>
                 <div className='contact-content w-[80%] shadow-2xl justify-center flex' style={{ height: '800px' }}>
+                    
+                    <div className='w-[100%] flex bg-[#FBEFF2]'>
 
-                    <div className='w-[100%] flex flex-col bg-[#FBEFF2] gap-10 p-5 justify-center'>
+                        <form className='w-[100%] flex flex-col gap-10 p-5 justify-center'>
 
-                        <h1 className='text-[#A10550] text-3xl text-center'>Register</h1>
-                        <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="email" name="name" placeholder='Email' id="" required />
-                        <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="text" name="text" placeholder='Username' id="" required />
-                        <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="text" name="role" placeholder='Role' id="" required />
-                        <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="password" name="password" placeholder='Password' id="" required />
+                            <h1 className='text-[#A10550] text-3xl text-center'>Register</h1>
+                            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="email" name="email" placeholder='Email' id="email" required />
+                            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="text" name="username" placeholder='Username' id="username" required />
+                            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="text" name="role" placeholder='Role' id="role" required />
+                            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' type="password" name="password" placeholder='Password' id="password" required />
             
-                        
-                        <button className='border-black border-2 p-3 w-[30%] mx-auto text-[#0C0C0C] shadow-xl'>Register</button>
+                            <button className='border-black border-2 p-3 w-[30%] mx-auto text-[#0C0C0C] shadow-xl'>Register</button>
+
+                        </form>
 
                     </div>
 
