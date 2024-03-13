13f0ddd9a3c608a8823e9a6f7644abc999092aca
Florian <florian.fani@hotmail.com>
2024-03-13 19:16:40 +0100

    register form ui with validation errors done

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -35,34 +35,34 @@ const Register = () => {
                     
                     <div className='w-[100%] flex bg-[#FBEFF2] '>
 
-                        <form onSubmit={formik.handleSubmit} className='w-[100%] flex flex-col gap-10 p-5 justify-center'>
+                        <form onSubmit={formik.handleSubmit} className='w-[100%] flex flex-col gap-10 justify-center'>
 
                             <h1 className='text-[#A10550] text-3xl text-center'>Register</h1>
 
                             <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto'
                             type="email" name="email" placeholder="Email" onChange={formik.handleChange} value={formik.values.email} />
                             {formik.errors.email && formik.touched.email && 
-                            <h2 className='text-red-500 text-sm'>{formik.errors.email}</h2>}
+                            <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.email}</h2>}
 
                             <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto'
                             type="text" name="username" placeholder="Username" onChange={formik.handleChange} value={formik.values.username} />
                             {formik.errors.username && formik.touched.username && 
-                            <h2 className='text-red-500 text-sm'>{formik.errors.username}</h2>}
+                            <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.username}</h2>}
 
                             <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto'
                             type="text" name="role" placeholder="Role" onChange={formik.handleChange} value={formik.values.role} />
                             {formik.errors.role && formik.touched.role && 
-                            <h2 className='text-red-500 text-sm'>{formik.errors.role}</h2>}
+                            <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.role}</h2>}
 
                             <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' 
                             type="password" name="password" placeholder="Password" onChange={formik.handleChange} value={formik.values.password} />
                             {formik.errors.password && formik.touched.password && 
-                            <h2 className='text-red-500 text-sm'>{formik.errors.password}</h2>}
+                            <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.password}</h2>}
 
                             <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' 
                             type="password" name="confirmPassword" placeholder="Confirm Password" onChange={formik.handleChange} value={formik.values.confirmPassword} />
                             {formik.errors.confirmPassword && formik.touched.confirmPassword && 
-                            <h2 className='text-red-500 text-sm'>{formik.errors.confirmPassword}</h2>}
+                            <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.confirmPassword}</h2>}
             
                             <button className='border-black border-2 p-3 w-[30%] mx-auto text-[#0C0C0C] shadow-xl'>Register</button>
 
