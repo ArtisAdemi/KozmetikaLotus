ef148bd9738ab7f17b83938355ae957f7e342a61
Florian <florian.fani@hotmail.com>
2024-03-17 20:48:38 +0100

    login page responsive ui done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -41,33 +41,33 @@ const Login = () => {
 
     return (
         <div>
-            <div className='contact-container w-full flex justify-center my-12'>
-                <div className='contact-content w-[80%] shadow-2xl justify-center flex' style={{ height: '800px' }}>
+            <div className='contact-container flex justify-center w-full my-12'>
+                <div className='contact-content w-[80%] shadow-2xl justify-center md:flex md:h-[800px]'>
                     
                     <div className='w-[100%] flex bg-[#FBEFF2] '>
 
-                        <form onSubmit={formik.handleSubmit} className='w-[100%] flex flex-col gap-10 justify-center'>
+                        <form onSubmit={formik.handleSubmit} className='w-[100%] flex flex-col gap-10 justify-center my-12'>
 
                             <h1 className='text-[#A10550] text-3xl text-center'>Login</h1>
 
-                            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto'
+                            <input className='rounded-md p-3 md:p-4 placeholder-gray-400 w-[60%] mx-auto'
                             type="email" name="email" placeholder="Email" onChange={formik.handleChange} value={formik.values.email} />
                             {formik.errors.email && formik.touched.email && 
                             <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.email}</h2>}
    
-                            <input className='rounded-md p-4 placeholder-gray-400 w-[60%] mx-auto' 
+                            <input className='rounded-md p-3 md:p-4 placeholder-gray-400 w-[60%] mx-auto' 
                             type="password" name="password" placeholder="Password" onChange={formik.handleChange} value={formik.values.password} />
                             {formik.errors.password && formik.touched.password && 
                             <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.password}</h2>}
                             
-                            <button type="submit" className='border-[#A10550] border-2 p-3 w-[30%] mx-auto text-[#A10550] shadow-xl'>Login</button>
+                            <button type="submit" className='border-[#A10550] border-2 p-3 md:p-4 w-[60%] mx-auto text-[#A10550] shadow-xl'>Login</button>
 
                         </form>
 
                     </div>
 
                     <div className='w-[100%] h-[100%]'>
-                      <img src={contact} alt="" className="w-[100%] h-[800px]"  />
+                      <img src={contact} alt="" className="md:w-[100%] md:h-[800px]"  />
                     </div>
           
                 </div>
