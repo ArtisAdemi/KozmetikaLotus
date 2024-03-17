d182afa08b2d27d2e1411a4d4a8f21e8d27af7cb
Florian <florian.fani@hotmail.com>
2024-03-17 19:49:01 +0100

    small category input ui update

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -52,27 +52,27 @@ const ContactUs = () => {
                         <form onSubmit={formik.handleSubmit} className='w-[100%] flex flex-col gap-10 justify-center my-12'>
                             <h1 className='text-[#A10550] text-3xl text-center'>Contact Us</h1>
 
-                            <input className='rounded-md p-3 placeholder-gray-400 w-[60%] mx-auto'
+                            <input className='rounded-md p-3 md:p-4 placeholder-gray-400 w-[60%] mx-auto'
                                 type="text" name="name" placeholder="Name" onChange={formik.handleChange} value={formik.values.name} />
                             {formik.errors.name && formik.touched.name &&
                                 <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.name}</h2>}
 
-                            <input className='rounded-md p-3 placeholder-gray-400 w-[60%] mx-auto'
+                            <input className='rounded-md p-3 md:p-4 placeholder-gray-400 w-[60%] mx-auto'
                                 type="email" name="email" placeholder="Email" onChange={formik.handleChange} value={formik.values.email} />
                             {formik.errors.email && formik.touched.email &&
                                 <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.email}</h2>}
 
-                            <input className='rounded-md p-3 placeholder-gray-400 w-[60%] mx-auto'
+                            <input className='rounded-md p-3 md:p-4 placeholder-gray-400 w-[60%] mx-auto'
                                 type="text" name="phone" placeholder="Phone" onChange={formik.handleChange} value={formik.values.phone} />
                             {formik.errors.phone && formik.touched.phone &&
                                 <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.phone}</h2>}
 
-                            <textarea style={{ resize: 'none' }} className='rounded-md p-3 placeholder-gray-400 w-[60%] mx-auto' cols="40" rows="5"
+                            <textarea style={{ resize: 'none' }} className='rounded-md p-3 md:p-4 placeholder-gray-400 w-[60%] mx-auto' cols="40" rows="5"
                                 type="textarea" name="message" placeholder="Write something..." onChange={formik.handleChange} value={formik.values.message} />
                             {formik.errors.message && formik.touched.message &&
                                 <h2 className='w-[60%] text-red-500 text-sm -mt-10 mx-auto'>{formik.errors.message}</h2>}
 
-                            <button type="submit" className='border-[#A10550] border-2 p-3 w-[60%] mx-auto text-[#A10550] shadow-xl'>Contact Us</button>
+                            <button type="submit" className='border-[#A10550] border-2 p-3 md:p-4 w-[60%] mx-auto text-[#A10550] shadow-xl'>Contact Us</button>
                         </form>
                     </div>
 
