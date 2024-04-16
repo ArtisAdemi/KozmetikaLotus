823ccc45019b4496c1ca8e2d2365df7342c84eed
Florian <florian.fani@hotmail.com>
2024-04-16 23:48:09 +0200

    formik preloaded user input fixed

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -29,9 +29,14 @@ const Checkout = () => {
         .required('Postal Code is required'),
       });
 
+      
       const formik = useFormik({
         initialValues: {
-            // User Data Values should come here..
+            // Preload user data into initialValues
+            firstName: user.firstName || '', // Ensure to handle cases where user.firstName might be undefined
+            lastName: user.lastName || '',
+            email: user.email || '',
+            phoneNumber: user.phoneNumber || '',
             city: '',
             address: '',
             postalCode: '',
@@ -47,6 +52,20 @@ const Checkout = () => {
       useEffect(() => {
         getUserData();
       }, [])
+
+      //Second useEffect is to handle the form changes -- (to set initial pre-loaded user data)
+      useEffect(() => {
+        // Set formik initialValues when user data changes
+        formik.setValues({
+            firstName: user.firstName || '',
+            lastName: user.lastName || '',
+            email: user.email || '',
+            phoneNumber: user.phoneNumber || '',
+            city: '',
+            address: '',
+            postalCode: '',
+        });
+    }, [user]); // Listen for changes in the user state
     
     return (
         <div>
@@ -65,31 +84,35 @@ const Checkout = () => {
                                 <div className='flex md:w-[40%] flex-col'>
 
                                     <form onSubmit={formik.handleSubmit}>
-        
+
                                         <h1 className='text-[#212121] pb-4 mb-8 border border-b-[#E0E0E0] border-l-0 border-r-0 border-t-0 font-semibold text-3xl'>
                                             Fatura e Adresimit
                                         </h1>
-        
+
                                         <div className='flex mb-5 justify-start items-center'>
-                                         <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Emri: </h2>
-                                            <h2 className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'>{user.firstName}</h2>
+                                            <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Emri: </h2>
+                                            <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
+                                             type="text" name='firstName' disabled onChange={formik.handleChange} value={formik.values.firstName} />
                                         </div>
-        
-                                       <div className='flex mb-5 justify-start items-center'>
-                                           <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Mbiemri: </h2>
-                                            <h2 className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'>{user.lastName}</h2>
+
+                                        <div className='flex mb-5 justify-start items-center'>
+                                            <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Mbiemri: </h2>
+                                            <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
+                                             type="text" name='lastName' disabled onChange={formik.handleChange} value={formik.values.lastName} />
                                         </div>
-        
+
                                         <div className='flex mb-5 justify-start items-center'>
                                             <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Email: </h2>
-                                            <h2 className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'>{user.email}</h2>
+                                            <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
+                                             type="text" name='email' disabled onChange={formik.handleChange} value={formik.values.email} />
                                         </div>
-        
+
                                         <div className='flex mb-5 justify-start items-center'>
                                             <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Nr. Telefonit: </h2>
-                                            <h2 className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'>{user.phoneNumber}</h2>
+                                            <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
+                                             type="text" name='phoneNumber' disabled onChange={formik.handleChange} value={formik.values.phoneNumber} />
                                         </div>
-                                     
+
                                         <div className='flex mb-5 justify-start items-center'>
                                             <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Qyteti: </h2>
                                             <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
@@ -97,7 +120,7 @@ const Checkout = () => {
                                         </div>
                                              {formik.errors.city && formik.touched.city && 
                                             <h2 className='w-[50%] md:w-[60%] text-red-500 text-xs md:text-sm -mt-4 mx-auto'>{formik.errors.city}</h2>}
-    
+
                                         <div className='flex mb-5 justify-start items-center'>
                                             <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Adresa: </h2>
                                             <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
@@ -105,7 +128,7 @@ const Checkout = () => {
                                         </div>
                                              {formik.errors.address && formik.touched.address && 
                                             <h2 className='w-[50%] md:w-[60%] text-red-500 text-xs md:text-sm -mt-4 mx-auto'>{formik.errors.address}</h2>}
-                                       
+
                                         <div className='flex justify-start mb-8 items-center'>
                                             <h2 className='w-[25%] md:w-[20%] text-sm md:text-base font-medium'>Kodi Postal: </h2>
                                             <input className='rounded-md w-[75%] md:w-[80%] text-sm md:text-base p-3 md:p-4 border bg-[#FBFCFDF0] border-[#E4E7EB]'
@@ -113,11 +136,11 @@ const Checkout = () => {
                                         </div>
                                              {formik.errors.postalCode && formik.touched.postalCode && 
                                             <h2 className='w-[50%] md:w-[60%] text-red-500 text-xs md:text-sm -mt-4 mx-auto'>{formik.errors.postalCode}</h2>}
-    
+
                                       <button type='submit' className='border-[#A3A7FC] bg-[#A3A7FC] rounded-md border-2 p-3 md:p-4 w-full md:w-[50%]  text-[#FFFFFF] shadow-xl hover:opacity-80'>
                                           Porosit
                                       </button>
-                                            
+
                                  </form>
 
                                 </div>
