0931f4ce94f13f9ed3391ff5dd33fa05f8dfadd8
ArtisAdemi <artis.ademi@gmail.com>
2024-05-19 01:33:19 +0200

    Profile bug fixed

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -2,15 +2,21 @@ import axios from 'axios';
 
 const AUTH_API_URL = 'http://localhost:3001/api/auth';
 
-const axiosInstance = axios.create({
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-});
+const axiosInstance = axios.create();
 
 const AuthService = {
+    setAuthToken: () => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        } else {
+            delete axiosInstance.defaults.headers.common['Authorization'];
+        }
+    },
+
     decodeUser: async () => {
         try {
+            AuthService.setAuthToken(); // Set the Authorization header
             const response = await axiosInstance.get(`${AUTH_API_URL}/getUserData`);
             return response.data;
         } catch (err) {
@@ -20,4 +26,4 @@ const AuthService = {
     }
 }
 
-export default AuthService
+export default AuthService;
